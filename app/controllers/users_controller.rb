93e623175c3a3100fbbdc466939f786d04059f53
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-19 22:20:28 +0900

    add create action and delete new and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,26 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @new_user = User.new
   end
 
   def show
     @user = User.find(params[:id])
   end
 
-  def new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "ユーザーを登録しました"
+    else
+      # render ""
+    end
   end
 
-  def edit
+  private def user_params
+    params.require(:user).permit(
+      :name,
+      :email
+    )
   end
 end
