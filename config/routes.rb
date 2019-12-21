96c0f0ca078261d38e1191fabe5d5adf7eea9106
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-22 07:43:46 +0900

    change routes of users to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
+  root "users#index"
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
