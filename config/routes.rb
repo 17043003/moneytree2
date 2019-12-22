5ac813ddb941c5d23e0f220bcc60c577822602b6
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-22 18:26:06 +0900

    add create and destroy action to session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users
+
+  resource :session, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
