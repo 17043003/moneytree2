46d34c77a62ba5ce9b16d1459a18fc7a12968565
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-22 17:07:43 +0900

    generate sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+
     validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
     validates :email, presence: true, email: true
 end
