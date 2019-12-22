9867c5402cfb5b7fc6bbcc4c9e7eae0c1bc0bb50
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-22 15:52:21 +0900

    add destroy action in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to :users, notice: "ユーザーを削除しました"
+  end
+
   private def user_params
     params.require(:user).permit(
       :name,
