499f69da26037b88435cff3aa37d151462119f60
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-22 23:29:38 +0900

    generate accounts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'accounts/show'
+  get 'accounts/edit'
   root "users#top"
 
   resources :users
 
   resource :session, only: [:create, :destroy]
+  resource :account, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
