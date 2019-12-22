afafb2a73505c9bfed66767bd7eada90dd02bd93
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-23 00:29:18 +0900

    generate passwords controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
     validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
     validates :email, presence: true, email: true
+
+    attr_accessor :current_password
+    validates :password, presence: { if: :current_password }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'passwords/edit'
   get 'accounts/show'
   get 'accounts/edit'
   root "users#top"
@@ -7,5 +8,6 @@ Rails.application.routes.draw do
 
   resource :session, only: [:create, :destroy]
   resource :account, only: [:show, :edit, :update]
+  resource :password, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
