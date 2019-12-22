e2e32ce6e44bf013c13028c7f35dd2e7d77f6531
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-23 00:26:54 +0900

    create account edit

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,28 @@
 class AccountsController < ApplicationController
+  before_action :login_required
+
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.assign_attributes(account_params)
+    if @user.save
+      redirect_to :account, notice: "アカウント情報を更新しました"
+    else
+      render "edit"
+    end
+  end
+
+  private def account_params
+    params.require(:account).permit(
+      :name,
+      :email
+    )
   end
 end
