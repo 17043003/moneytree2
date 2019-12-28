3855c2b1cb8061ad84405fe0ebf1c41f92b24217
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-28 16:51:15 +0900

    migrate expense table

--- db/migrate/20191228074442_create_expenses.rb
+++ db/migrate/20191228074442_create_expenses.rb
@@ -1,6 +1,9 @@
 class CreateExpenses < ActiveRecord::Migration[5.2]
   def change
     create_table :expenses do |t|
+      t.references :user, null: false
+      t.references :budget, null: false
+      t.integer :amount, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_071001) do
+ActiveRecord::Schema.define(version: 2019_12_28_074442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,26 @@ ActiveRecord::Schema.define(version: 2019_12_28_071001) do
     t.string "kind", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "budget_id", null: false
+    t.integer "amount", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["budget_id"], name: "index_expenses_on_budget_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
+
+  create_table "expenses_by_categories", force: :cascade do |t|
+    t.bigint "budget_id", null: false
+    t.bigint "category_id", null: false
+    t.integer "expense", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["budget_id"], name: "index_expenses_by_categories_on_budget_id"
+    t.index ["category_id"], name: "index_expenses_by_categories_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
