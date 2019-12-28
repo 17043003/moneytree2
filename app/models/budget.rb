8762e22cf17b2711305359dcfd85971445946e51
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-28 16:57:09 +0900

    add has_many and belong_to to budget, category and expense models

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,5 +1,7 @@
 class Budget < ApplicationRecord
     belongs_to :user
+    has_many :expenses, dependent: :destroy
+    has_many :categories, through: :expenses
 
     validates :spent_at, presence: true
     validates :amount, presence: true, numericality: {only_integer: true}
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+    has_many :expenses, dependent: :destroy
+    has_many :budgets, through: :expenses
+
     validates :kind, presence: true
 end
