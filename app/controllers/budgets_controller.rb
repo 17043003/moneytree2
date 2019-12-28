e49610f73e478f2704359cc7cf7109d70fc542ce
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-28 11:40:21 +0900

    can select date in index view

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,14 +2,31 @@ class BudgetsController < ApplicationController
   before_action :login_required
 
   def index
-    # @budgets = Budget.all
-    # searched_date = Date.today
-    # @budgets = Budget.where(spent_at: searched_date.in_time_zone.all_month).order(:spent_at)
-
     @budgets = []
-    year = 2019
-    month = 12
-    (1..31).each do |day| 
+    year = params["display_date(1i)"]&.to_i # 日付の範囲設定で使用するためto_iで数値にする
+    month = params["display_date(2i)"]&.to_i
+
+    # 最初に現在の年,月を表示する
+    if year == nil && month == nil
+      year = Time.now.year
+      month = Time.now.month
+      @selected_date = Time.new(year, month, 1) # indexの見出し,日付プルダウンのデフォルト値に使用する
+    else
+      @selected_date = Time.new(year, month, 1)
+    end
+
+    # 表示する日付の範囲を設定
+    if [1, 3, 5, 7, 8, 10, 12].include?(month)
+      display_day_range = (1..31)
+    elsif month == 2
+      display_day_range = (1..28) if year%4 != 0
+      display_day_range = (1..29) if year%4 == 0 # うるう年
+    else
+      display_day_range = (1..30) # 4,6,9,11月
+    end
+
+    # データベースに存在しない日付も配列に入れる
+    display_day_range.each do |day| 
       if Budget.find_by(spent_at: Time.new(year, month, day))
         @budgets.push(Budget.find_by(spent_at: Time.new(year, month, day)))
       else
