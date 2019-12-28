222fd919b91a90460570d21777e8aebd8670279c
17043003 <35760051+17043003@users.noreply.github.com>
2019-12-28 12:38:49 +0900

    add user_id to budgets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_121603) do
+ActiveRecord::Schema.define(version: 2019_12_28_033745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_12_26_121603) do
     t.string "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2019_12_26_121603) do
     t.string "password_digest"
   end
 
+  add_foreign_key "budgets", "users"
 end
